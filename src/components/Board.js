6e0eb5b0c85c1139067bec8fae992a0b9b31fffa
kawgh1 <krseven@gmail.com>
2021-08-26 20:50:30 -0500

    set up basic components Board and Square and root level styling

---
+++ src/components/Board.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Square from './Square'
+
+const Board = () => {
+    return (
+        <div className='board'>
+
+            <div className='board-row'>
+                <Square value={0}/>
+                <Square />
+                <Square />
+            </div>
+
+            <div className='board-row'>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+
+            <div className='board-row'>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </div>
+    )
+};
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Square = () => {
+    return <button type='button' className='square'>1</button>
+};
+
+export default Square;
